369d4d6231fe5a619c343b3c1b7b43bbea2d7801
hafizhalmaarifkarimamrullah <hafizhalmaarifka@gmail.com>
2023-12-08 09:28:33 +0700

    first commit

---
+++ src/main/java/com/tmsuper/websocket/chat/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.tmsuper.websocket.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private MessageType type;
+    private String content;
+    private String sender;
+    private String baseimage;
+
+}
---
+++ src/main/java/com/tmsuper/websocket/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.tmsuper.websocket.chat;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
